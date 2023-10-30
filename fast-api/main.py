17d876b2c196c773617d3e683f067bd2c5127ba3
Alun Price <alunprice089@gmail.com>
2023-10-30 21:11:40 +0000

    21:29 Initial commit of files

---
+++ fast-api/main.py
@@ -0,0 +1,45 @@
+import json
+import consumers
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+
+app = FastAPI()
+
+# to allow backend-frontend connection without cors issue
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=['http://localhost:3000'],
+  allow_methods=['*'],
+  allow_headers=['*']
+)
+
+redis = get_redis_connection(
+  host='redis-18530.c268.eu-west-1-2.ec2.cloud.redislabs.com',
+  port=18530,
+  password="fODfnRpIlRU4E9wtMxyB0PhVs9echIAu",
+  decode_responses=True
+)
+
+class Delivery(HashModel):
+  budget: int = 0
+  notes: str = ''
+  
+  class Meta: 
+    database = redis
+    
+class Event(HashModel):
+  delivery_id: str = None
+  type: str
+  data: str
+  
+  class Meta: 
+    database = redis
+
+@app.post('/deliveries/create')
+async def create(request: Request):
+  body = await request.json()
+  delivery = Delivery(budget=body['data']['budget'], notes=body['data']['notes']).save()
+  event = Event(delivery_id=delivery.pk, type=body['type'], data=json.dumps(body['data'])).save()
+  state = consumers.create_delivery({}, event)
+  return state
