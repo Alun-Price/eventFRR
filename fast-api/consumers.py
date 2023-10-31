3b49c74f4fe0c3e6622aefe16a0a3ac186694bd0
Alun Price <alunprice089@gmail.com>
2023-10-31 03:52:15 +0000

    41:27 Add pickup_products function

--- fast-api/consumers.py
+++ fast-api/consumers.py
@@ -1,5 +1,7 @@
 import json
 
+from fastapi import HTTPException
+
 def create_delivery(state, event):
   data = json.loads(event.data)
   return {
@@ -11,5 +13,25 @@ def create_delivery(state, event):
   
 def start_delivery(state, event):
   return state | {
-    "status":"active"
+    "status": "active"
   }
+  
+def pickup_products(state, event):
+  data = json.loads(event.data)
+  new_budget = state["budget"] - int(data['purchase_price']) * int(data['quantity'])
+
+  if new_budget < 0:
+      raise HTTPException(status_code=400, detail="Not enough budget")
+
+  return state | {
+      "budget": new_budget,
+      "purchase_price": int(data['purchase_price']),
+      "quantity": int(data['quantity']),
+      "status": "collected"
+  }
+
+CONSUMERS = {
+  "CREATE_DELIVERY":create_delivery,
+  "START_DELIVERY":start_delivery,
+  "PICKUP_PRODUCTS":pickup_products 
+}
