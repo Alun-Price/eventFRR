93d79b97e2b3d0d346e1b3778f8a0c58682625ef
Alun Price <alunprice089@gmail.com>
2023-10-31 04:09:30 +0000

    Fix new budget calculation

--- fast-api/consumers.py
+++ fast-api/consumers.py
@@ -6,7 +6,7 @@ def create_delivery(state, event):
   data = json.loads(event.data)
   return {
     "id": event.delivery_id,
-    "budget": int(data['budget']),
+    "budget": int(data["budget"]),
     "notes": data["notes"],
     "status":"ready"
   }
