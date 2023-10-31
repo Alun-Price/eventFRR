d58ee4e2f1c6960ded8fa4e093cde67a68dbf896
Alun Price <alunprice089@gmail.com>
2023-10-31 21:53:30 +0000

    54:33 Added status and quantity control HTTPException code

--- fast-api/consumers.py
+++ fast-api/consumers.py
@@ -12,6 +12,8 @@ def create_delivery(state, event):
   }
   
 def start_delivery(state, event):
+  if state['status'] != 'ready':
+    raise HTTPException(status_code=400, detail="Delivery already started")
   return state | {
     "status": "active"
   }
@@ -35,6 +37,9 @@ def deliver_products(state, event):
   new_budget = state["budget"] + int(data['sell_price']) * int(data['quantity'])
   new_quantity = state['quantity'] - int(data['quantity'])
 
+  if new_quantity < 0:
+      raise HTTPException(status_code=400, detail="Not enough quantity")
+  
   return state | {
       "budget": new_budget,
       "sell_price": int(data['sell_price']),
@@ -42,9 +47,15 @@ def deliver_products(state, event):
       "status": "completed"
   }
 
+def increase_budget(state, event):
+  data = json.loads(event.data)
+  state['budget'] += int(data['budget'])
+  return state
+
 CONSUMERS = {
-  "CREATE_DELIVERY":create_delivery,
-  "START_DELIVERY":start_delivery,
-  "PICKUP_PRODUCTS":pickup_products, 
-  "DELIVER_PRODUCTS":deliver_products, 
+  "CREATE_DELIVERY": create_delivery,
+  "START_DELIVERY": start_delivery,
+  "PICKUP_PRODUCTS": pickup_products, 
+  "DELIVER_PRODUCTS": deliver_products,
+  "INCREASE_BUDGET": increase_budget, 
 }
